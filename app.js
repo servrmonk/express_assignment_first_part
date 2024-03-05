f9dda042350d6ac420c0ad5cf2ceb0c40772311e
Raushan <roushankumarsingh2018@gmail.com>
2024-03-05 13:21:07 +0530

    use bodyparser to parse the data

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const http = require("http");
 
+const bodyParser = require('body-parser')
+
 const app = express();
-app.use((req, res, next) => {
-  //use allows us to add a new middleware function,use method is preety flexible
-  console.log("In the middleware ");
-  // use next for next middleware
-  next()//allows the request to continue to the next middleware in line
+
+
+app.use(bodyParser.urlencoded());//this will parse the form with this ,here automatically next 
+
+app.use("/", (req, res, next) => {
+// console.log("This always runs");
+next();
 });
-app.use((req, res, next) => {
-  console.log("In the second middleware ");
-  // instead of doing res.write we will use res.send this allows us to add a body which is of type any
-  res.send('<h1>Hello From Express</h1>')
+
+app.use("/add-product", (req, res, next) => {
+  // console.log("Inside add-product routes");
+  res.send(`<form action='/product' method="POST"><input type='text' name='title'><button type='submit'>Add Product</button></form>`);
 });
-app.use((req,res,next)=>{
-  console.log("In third middleware")
-  res.send({ key1: value });
+  
+app.use('/product',(req,res,next)=>{
+  // getting the body of my incoming request  so extracting what the user sents me  and for this express.js now have convinience features for that
+  console.log(req.body);
+  // we will get the value undefined ,but by default request doesn't try to parse the incoming request body to do that we need to register parser and to do that by adding new middleware, and u typically do that before your route handling middlewares because the parsing of the middleware done no matter where ur request ends up for that weneed to install the package npm i --save body-parser
+  
+  res.redirect('/'); //i can redirect to anything
 })
 
+app.use("/", (req, res, next) => {
+  // console.log("In the middleware ");
+  res.send("<h1>Hello from express!</h1>");
+});
 
-// const server = http.createServer(app);
-// server.listen(3000,()=>console.log("Server is listening"));
-
-app.listen(3000,()=>{console.log("Server listening");})
+app.listen(3000, () => {
+  console.log("Server listening");
+});
