95bdafe74b446fd203b280d6a2634cbcf34dbd26
Raushan <roushankumarsingh2018@gmail.com>
2024-03-05 13:25:18 +0530

    uses app.post and app.get

--- app.js
+++ app.js
@@ -18,13 +18,24 @@ app.use("/add-product", (req, res, next) => {
   res.send(`<form action='/product' method="POST"><input type='text' name='title'><button type='submit'>Add Product</button></form>`);
 });
   
-app.use('/product',(req,res,next)=>{
-  // getting the body of my incoming request  so extracting what the user sents me  and for this express.js now have convinience features for that
-  console.log(req.body);
-  // we will get the value undefined ,but by default request doesn't try to parse the incoming request body to do that we need to register parser and to do that by adding new middleware, and u typically do that before your route handling middlewares because the parsing of the middleware done no matter where ur request ends up for that weneed to install the package npm i --save body-parser
+// app.use('/product',(req,res,next)=>{
+   // getting the body of my incoming request  so extracting what the user sents me  and for this express.js now have convinience features for that
+//   console.log(req.body);
+   // we will get the value undefined ,but by default request doesn't try to parse the incoming request body to do that we need to register parser and to do that by adding new middleware, and u typically do that before your route handling middlewares because the parsing of the middleware done no matter where ur request ends up for that weneed to install the package npm i --save body-parser
   
-  res.redirect('/'); //i can redirect to anything
+//   res.redirect('/'); //i can redirect to anything
+// })
+// we can use app.get also , app.get allows u to filter for get request, that u can also use app.post
+// instead of using use we can also use post and get which is usee to filter
+app.post('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
 })
+
+// app.get('/product',(req,res,next)=>{
+//   console.log(req.body);
+//   res.redirect('/')
+// })
 
 app.use("/", (req, res, next) => {
   // console.log("In the middleware ");
