d41ddca9f0a16205089f14cb6283fecd9b36b2ea
Raushan <roushankumarsingh2018@gmail.com>
2024-03-08 14:25:55 +0530

    used path ,__dirname and other stuff

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
+const path = require('path')
 
-router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<form action='/admin/add-product' method="POST"><input type='text' required name='title'><input type='number' required name='size'><button type='submit' />Add Product</button></form>`
-  );
+
+router.get("/", (req, res, next) => {
+  // res.send(
+  //   `<form action='/admin/add-product' method="POST"><input type='text' required name='title'><input type='number' required name='size'><button type='submit' />Add Product</button></form>`
+  // );
+
+  res.sendfile(path.join(__dirname,'../','views','add-product.html'));
+
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,18 @@
 const express = require("express");
 const router = express.Router(); //router object
 
-router.get("/shop-page", (req, res, next) => {
-  res.send("<h1>Hello from express!</h1>");
+const path  = require('path')
+
+router.get("/", (req, res, next) => {
+  // res.send("<h1>Hello from express!</h1>");
+  // instead of sending the text we will send the file
+  // res.sendFile('./views/shop.html')//see from the view of app.js. this will refer to our operating system not to the project folder
+  // whatever we try upper one will not work we will have to use path from nodejs,so in order to construct the folder to this directory or this path ultimately we can use the feature provided by node js and that is path, we have to import path
+
+  // res.sendFile(path.join(__dirname,'views','shop.html')) //we call join method  , joins return a paths but it constructs the path by concating the different segments. __dirname will holdthe absolute paths on our operating system to this project folder and now we can add commma and addd views folder and than file name, here  __dirname gives us the path to the routes folder  ,dirname gives us the path to the file which we use this we will add ../ one new segment that means go up  one label from here
+
+  res.sendFile(path.join(__dirname,'../','views','shop.html'))
+
+
 });
 module.exports = router;
