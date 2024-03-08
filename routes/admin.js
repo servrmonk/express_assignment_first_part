b78a9cca492456179fbb3f18a7854d32fdcae2de
Raushan <roushankumarsingh2018@gmail.com>
2024-03-08 14:35:06 +0530

    added 404 not found page

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const path = require('path')
 
 
-router.get("/", (req, res, next) => {
-  // res.send(
-  //   `<form action='/admin/add-product' method="POST"><input type='text' required name='title'><input type='number' required name='size'><button type='submit' />Add Product</button></form>`
-  // );
+router.get("/add-product", (req, res, next) => {
+ 
 
-  res.sendfile(path.join(__dirname,'../','views','add-product.html'));
+  res.sendFile(path.join(__dirname,'../','views','add-product.html'));
 
 });
 
