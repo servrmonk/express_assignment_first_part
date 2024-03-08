236bb9a8eb7d75b5f80710e79cb8f7edd913de9f
Raushan <roushankumarsingh2018@gmail.com>
2024-03-08 14:37:00 +0530

    we can also use .. instead of ../

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path')
-
+const path = require("path");
 
 router.get("/add-product", (req, res, next) => {
- 
-
-  res.sendFile(path.join(__dirname,'../','views','add-product.html'));
-
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html")); //we can also use .. instead of ../
 });
 
 router.post("/add-product", (req, res, next) => {
