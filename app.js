68a5986aa5c678c516d479fce24e24e40f57769a
Raushan <roushankumarsingh2018@gmail.com>
2024-03-08 10:05:56 +0530

    just clearing some comments

--- app.js
+++ app.js
@@ -1,45 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const http = require("http");
+
 const app = express();
 
 const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
-// the request goes from top to buttom 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes); //starting with admin will go into admin routes file so to say and not only that
-app.use('/shop',shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/shop", shopRoutes);
 
-// app.use("/", (req, res, next) => {
-// console.log("This always runs");
-//   next();
-// });
-//outsourcing routing: in routes folder
-
-// app.use("/add-product", (req, res, next) => {
-//   res.send(
-//     `<form action='/product' method="POST"><input type='text' required name='title'><input type='number' required name='size'><button type='submit' />Add Product</button></form>`
-//   );
-// });
-
-// app.post("/product", (req, res, next) => {
-//   console.log(req.body);
-//   res.redirect("/");
-// });
-
-// app.use("/", (req, res, next) => {
-//   res.send("<h1>Hello from express!</h1>");
-// });
-
-
-// to add a 404 page we simply add a catch all middleware at the bottom where we not need the path  filter but we could add slash, app.use handle all http method not just get request 
-app.use((req,res,next)=>{
-  
-res.status(404).send("<h1>Page not found</h1>") //404 for page no found
-})
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Server listening");
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,6 @@
-// because this should handle the creation of the product which the admin of the shop can do
-
 const express = require("express");
-// the feature is router
-const router = express.Router(); //this router is like a mini express app tied to the other express app which we can export here
 
-// admin/add-product => GET request
+const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
@@ -12,10 +8,7 @@ router.get("/add-product", (req, res, next) => {
   );
 });
 
-// admin/add-product => POST request
-
-router.post("/add-product", (req, res, next) => { //instead of adding /admin in this page again and again we can dirctly add in app.ja
-    // router.post("/admin/add-product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
