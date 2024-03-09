bdb8eb6930f287b326c31eb9bba166d99473e8bb
Raushan <roushankumarsingh2018@gmail.com>
2024-03-09 15:06:03 +0530

    removed comment

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'public'))) //express object itself and static  method this is the built in middleware as u can read , here u serve content statically for css ,it serves static files, we will giving the acces to the public folder so we have written the dirnameis public
+app.use(express.static(path.join(__dirname,'public'))) 
 
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 
 app.use((req, res, next) => {
   
-  // res.status(404).send("<h1>Page not found</h1>");
+  
   res.status(404).sendFile(path.join(rootDir,'views','404.html'))
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,11 @@
 // what the user see
 const express = require("express");
-const router = express.Router(); //router object
+const router = express.Router();
 const path = require("path");
 
-const roodDir = require('../util/path')
-
-
-
+const roodDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(roodDir, "views", "shop.html")); //we can also use .. instead of ../
+  res.sendFile(path.join(roodDir, "views", "shop.html")); 
 });
 module.exports = router;
